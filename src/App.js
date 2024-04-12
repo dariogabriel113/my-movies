a43bb6f6dd02c45e503b2455436bb74d7368158a
Dario Gabriel <dariogabriel113@gmail.com>
2024-04-12 17:40:28 -0300

    Adiciona busca de filmes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react";
 import { Header } from "./components/Header";
 import { MoviesList } from "./components/MoviesList";
 import { MoviesContainer } from "./styles/MoviesContainer";
+import { MoviesService } from "./api/MoviesService";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
+    const {data} = await MoviesService.getMovies();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <div className="App">
       <Header />
 
       <MoviesContainer>
-        <MoviesList movies={[]} />
+        <MoviesList movies={movies} />
       </MoviesContainer>
     </div>
   );
---
+++ src/api/MoviesService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//TODO - Incluir apiKey
+const API_KEY = '';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const withBaseUrl = path => `${BASE_URL}${path}?api_key=${API_KEY}`;
+
+export class MoviesService {
+    static getMovies() {
+        return axios(withBaseUrl('movie/popular'))
+    }
+}
